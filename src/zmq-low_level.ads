c328826e64c4f934d1bee87148439c72b6a01e24
Per Sandberg <per.sandberg@bredband.net>
2010-12-07 08:05:17 +0100

    Licence is now MIT/X11 compatible

--- src/zmq-low_level.ads
+++ src/zmq-low_level.ads
@@ -1,34 +1,34 @@
-------------------------------------------------------------------------------
---                                                                          --
---                             0MQ Ada-binding                              --
---                                                                          --
---                         Z M Q . L O W _ L E V E L                        --
---                                                                          --
---                                  S p e c                                 --
---                                                                          --
---            Copyright (C) 2010-2011, per.sandberg@bredband.net            --
---                                                                          --
--- 0MQ Ada-binding is free software;  you can  redistribute it  and/or      --
--- modify it under terms of the  GNU General Public License as published    --
--- by the Free Soft-ware  Foundation;                                       --
--- either version 2,  or (at your option) any later version.                --
--- 0MQ Ada-binding is distributed in the hope that it will be useful, but   --
--- WITH OUT ANY WARRANTY;                                                   --
--- without even the  implied warranty of MERCHANTABILITY or                 --
--- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    --
--- for  more details.  You should have  received  a copy of the GNU General --
--- Public License  distributed with GNAT;  see file COPYING.  If not, write --
--- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
--- Boston, MA 02110-1301, USA.                                              --
---                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
---                                                                          --
-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--                                                                           --
+--                             0MQ Ada-binding                               --
+--                                                                           --
+--                         Z M Q . L O W _ L E V E L                         --
+--                                                                           --
+--                                  S p e c                                  --
+--                                                                           --
+--            Copyright (C) 2010-2011, per.sandberg@bredband.net             --
+--                                                                           --
+--  Permission is hereby granted, free of charge, to any person obtaining a  --
+--  copy of this software and associated documentation files                 --
+--  (the "Software"), to deal in the Software without restriction, including --
+--  without limitation the rights to use, copy, modify, merge, publish,      --
+--  distribute, sublicense, and / or sell copies of the Software, and to     --
+--  permit persons to whom the Software is furnished to do so, subject to    --
+--  the following conditions :                                               --
+--                                                                           --
+--  The above copyright notice and this permission notice shall be included  --
+--  in all copies or substantial portions of the Software.                   --
+--                                                                           --
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  --
+--  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF               --
+--  MERCHANTABILITY,                                                         --
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL  --
+--  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR     --
+--  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,    --
+--  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR    --
+--  OTHER DEALINGS IN THE SOFTWARE.                                          --
+-------------------------------------------------------------------------------
+
 
 --  The contents of this file is derived from zmq.h using the
 --   -fdump-ada-spec switch for gcc.
