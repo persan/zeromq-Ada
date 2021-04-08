651ca44cce831c2d717338eab65a60fbfca7ec44
persan <per.s.sandberg@bahnhof.se>
2021-04-08 16:22:36 +0200

    Update license

--- fixHeader.py
+++ fixHeader.py
@@ -29,35 +29,32 @@ import string
 
 HEADER = \
 """##############################################################################
-##                                                                          ##
+##                              MIT License                                 ##
 ##                             0MQ Ada-binding                              ##
-##                                                                          ##
 %(name)s
 ##                                                                          ##
 %(ext)s
 ##                                                                          ##
-##            Copyright (C) 2013-2020, per.s.sandberg@bahnhof.se            ##
+##             Copyright (c) 2021 per.s.sandberg@bahnhof.se                 ##
 ##                                                                          ##
-## 0MQ Ada-binding is free software;  you can  redistribute it  and/or      ##
-## modify it under terms of the  GNU General Public License as published    ##
-## by the Free Soft-ware  Foundation;                                       ##
-## either version 2,  or (at your option) any later version.                ##
-## 0MQ Ada-binding is distributed in the hope that it will be useful, but   ##
-## WITH OUT ANY WARRANTY;                                                   ##
-## without even the  implied warranty of MERCHANTABILITY or                 ##
-## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    ##
-## for  more details.  You should have  received  a copy of the GNU General ##
-## Public License  distributed with GNAT;  see file COPYING.  If not, write ##
-## to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, ##
-## Boston, MA 02110-1301, USA.                                              ##
+## Permission is hereby granted, free of charge, to any person obtaining a  ##
+## copy of this software and associated documentation files                 ##
+## (the "Software"), to deal in the Software without restriction, including ##
+## without limitation the rights to use, copy, modify, merge, publish,      ##
+## distribute, sublicense, and/or sell copies of the Software, and to       ##
+## permit persons to whom the Software is furnished to do so, subject to    ##
+## the following conditions:                                                ##
 ##                                                                          ##
-## As a special exception,  if other files  instantiate  generics from this ##
-## unit, or you link  this unit with other files  to produce an executable, ##
-## this  unit  does not  by itself cause  the resulting  executable  to  be ##
-## covered  by the  GNU  General  Public  License.  This exception does not ##
-## however invalidate  any other reasons why  the executable file  might be ##
-## covered by the  GNU Public License.                                      ##
+## The above copyright notice and this permission notice shall be included  ##
+## in all copies or substantial portions of the Software.                   ##
 ##                                                                          ##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  ##
+## OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF               ##
+## MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   ##
+## IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY     ##
+## CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,     ##
+## TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        ##
+## SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   ##
 ##############################################################################
 """
 
@@ -101,16 +98,16 @@ def fixFile(f, of):
         begin = begin.forward_line(1)
     last = ed.beginning_of_buffer().search(r"\n\n", regexp=True)
 
-    of.write("%s\n" % 80 * "/")
-    of.write("%s\n" % f.name())
-    of.write("%s\n" % 30 * "<")
-    of.write("%s\n" % ed.get_chars(begin, last[0]))
-    of.write("%s\n" % 30 * "<")
-    of.write("%s\n" % getHeader(f))
-    of.write("%s\n" % 80 * "/")
+    of.write("%s\n" % ( 80 * "/"))
+    of.write("%s\n" % (f.name()))
+    of.write("%s\n" % (30 * "<"))
+    of.write("%s\n" % (ed.get_chars(begin, last[0])))
+    of.write("%s\n" % (30 * "<"))
+    of.write("%s\n" % (getHeader(f)))
+    of.write("%s\n" % (80 * "/"))
 
 with file("temp.out", "w") as of:
-    for i in GPS.Project("zmq.tests").sources():
+    for i in GPS.Project("zmq").sources():
         fixFile(i, of)
 
 
